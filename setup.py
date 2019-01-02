56f68ac5a5c155dd7fd27a7dd3b20e54041c6d58
Amit <>
2019-01-03 00:22:56 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='markovian-dynamics',
+    version='0.1',
+    packages=['markoviandynamics', 'markoviandynamics.numeric', 'markoviandynamics.plotting',
+              'markoviandynamics.symbolic'],
+    url='https://github.com/amitg7/markovian-dynamics',
+    license='MIT',
+    author='Amit',
+    author_email='',
+    description='Markovian-dynamics is a python package built to simplify dynamics calculations in thermodynamic systems.'
+)
